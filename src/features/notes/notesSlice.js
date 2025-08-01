7eb66cd7e5a9bad1984d58b5ab4dadc6e6fc3978
Dibesh Ranjan <dibeshranjan6@gmail.com>
2025-08-01 23:25:50 +0530

    update complete

--- src/features/notes/notesSlice.js
+++ src/features/notes/notesSlice.js
@@ -1,17 +1,27 @@
-
 import { createSlice } from '@reduxjs/toolkit';
+
+const saveToLocalStorage = (notes) => {
+  localStorage.setItem('notes', JSON.stringify(notes));
+};
+
+const loadFromLocalStorage = () => {
+   return JSON.parse(localStorage.getItem('notes')) || []
+};
+
 
 const notesSlice = createSlice({
   name: 'notes',
   initialState: {
-    notes: [],
+    notes: loadFromLocalStorage(),
   },
   reducers: {
     addNote: (state, action) => {
       state.notes.push(action.payload);
+      saveToLocalStorage(state.notes);
     },
     deleteNote: (state, action) => {
       state.notes = state.notes.filter((note) => note.id !== action.payload);
+      saveToLocalStorage(state.notes);
     },
     updateNote: (state, action) => {
       const { id, title, note } = action.payload;
@@ -19,6 +29,7 @@ const notesSlice = createSlice({
       if (existingNote) {
         existingNote.title = title;
         existingNote.note = note;
+        saveToLocalStorage(state.notes);
       }
     },
   },
@@ -26,3 +37,4 @@ const notesSlice = createSlice({
 
 export const { addNote, deleteNote, updateNote } = notesSlice.actions;
 export default notesSlice.reducer;
+
